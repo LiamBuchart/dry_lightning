7c99443059758c62f238d87abf1d06a9ffe617c9
Buchart <liam.buchart@nrcan-rncan.gc.ca>
2026-04-23 14:55:51 -0600

    update conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,9 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +26,34 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'furo'
 html_static_path = ['_static']
+
+html_theme_options = {
+    'light_css_variables': {
+        'color-brand-primary': '#0969da',
+        'color-brand-content': '#0969da',
+        'font-stack': '"Segoe UI", "Helvetica Neue", sans-serif',
+        'font-stack--monospace': '"SFMono-Regular", "Courier New", monospace',
+    },
+    'dark_css_variables': {
+        'color-brand-primary': '#58a6ff',
+        'color-brand-content': '#58a6ff',
+    },
+}
+
+# -- Path setup --------------------------------------------------------------
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+#import os
+#import sys
+#sys.path.insert(0, os.path.abspath('..'))
+#sys.path.insert(0, os.path.abspath('../../'))
+
+# copy d0.png, d1.png, and d1_yesterday.png to the _static directory
+import shutil
+shutil.copy('../../FORECAST/MAPS/d0.png', '_static/d0.png')
+shutil.copy('../../FORECAST/MAPS/d1.png', '_static/d1.png')
+shutil.copy('../../FORECAST/MAPS/d1_yesterday.png', '_static/d1_yesterday.png')
